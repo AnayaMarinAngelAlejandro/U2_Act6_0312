93ec8767f99e4501592fc233284c9816ac8bc962
ANGEL_ALEJANDRO_ANAYA_MARIN <a.21308051280312@cbtis128.edu.mx>
2024-04-24 15:30:40 +0000

    Actualizados

---
+++ lib/data/product_data.dart
@@ -0,0 +1,16 @@
+import 'package:anayaact6/models/product_model.dart';
+
+List<ProductModel> products = [
+  ProductModel(
+      img: "assets/images/img1.jpg",
+      title: "Camisa 1",
+      description: "Camisa manga corta 1"),
+  ProductModel(
+      img: "assets/images/img2.jpg",
+      title: "Camisa 2",
+      description: "Camisa manga corta 2"),
+  ProductModel(
+      img: "assets/images/img3.jpg",
+      title: "Camisa 3",
+      description: "Camisa manga corta 3"),
+];
